10bcd1efd3536dbfcfb2a3c16474ea43f70ada47
Ashi <ayeshashraf318@gmail.om>
2024-03-21 17:31:19 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let greet = "hello";
+if (greet == "hemlo")
+    console.log("heeloooowwww kesey hooo");
+else if (greet == "hemlo")
+    console.log("aey chmmar");
+else
+    console.log("teko kya hy kam krna apna");
+export {};
